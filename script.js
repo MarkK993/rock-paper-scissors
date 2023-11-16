ff81b646619c8fa7f2fd49b17bbf6e80c3fe672b
Marko Kozul <marko.kozul993@gmail.com>
2023-11-16 19:42:38 +0100

    Add random choice function

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Create a function getComputerChoice (check it with console.log);
 function getComputerChoice() {
-
+    const choice = ["Rock", "Paper", "Scissors"];
+    return choice[(Math.floor(Math.random() * choice.length))]
+    
 };
+console.log(getComputerChoice())
 // Create a function that plays a single round;Needs to have 2 parameters "playerSelection" and "computerSelection";
 function playRound(playerSelection, computerSelection) {
     return "Not even close!!! Try again";
