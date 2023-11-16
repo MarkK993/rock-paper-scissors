663ec7caaa04d3ad44207bc78a93831c241eb60a
Marko Kozul <marko.kozul993@gmail.com>
2023-11-16 19:10:37 +0100

    Add function

---
+++ script.js
@@ -0,0 +1,17 @@
+// Create a function getComputerChoice (check it with console.log);
+function getComputerChoice() {
+
+};
+// Create a function that plays a single round;Needs to have 2 parameters "playerSelection" and "computerSelection";
+function playRound(playerSelection, computerSelection) {
+    return "Not even close!!! Try again";
+};
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice;
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    
+};
+
