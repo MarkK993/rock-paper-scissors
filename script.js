c4cf067b94332be3e2c8e66c4221cc64bcad8a21
Marko Kozul <marko.kozul993@gmail.com>
2023-12-21 11:59:46 +0100

    Added UI to rps game

--- script.js
+++ script.js
@@ -1,111 +1,160 @@
-// Create a function getComputerChoice (check it with console.log);
-function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"];
-    // Math.random() * choice.length provides a number between 0 and length and than Math.floor rounds it up and makes a choice in array to return!!!!
-    return choice[(Math.floor(Math.random() * choice.length))]
-};
+let playerScore = 0;
+let computerScore = 0;
 
-// Create a function that plays a single round;Needs to have 2 parameters "playerSelection" and "computerSelection";
+// Function to get computer's choice
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+// Function to play a single round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie!";
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return "You need more practice young padawan";
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You won this round"; 
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You won this round"; 
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return "You need more practice young padawan"; 
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You won this round"; 
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You need more practice young padawan"; 
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        return "You won this round";
     } else {
-        return "You chose wrong weapon";
+        return "You need more practice, young padawan";
     }
-}; 
+}
 
+// Function to get user input
+function getUserSelection() {
+    let selection = prompt("Choose your weapon wisely!\nRock, Paper, or Scissors are available");
 
-// function to get user input and apply transformations
-    function getUserSelection() {
-        let selection = prompt("Choose your weapon wisely!\nRock, Paper, or Scissors are available");
+    if (selection === null) {
+        console.log("Player canceled. Exiting game.");
+        return null;
+    }
 
-        // Check for null and undefined
-    if (selection === null || selection === undefined) {
+    const normalizedSelection = selection.trim().toLowerCase();
+
+    if (!["rock", "paper", "scissors"].includes(normalizedSelection)) {
         console.log("Invalid input. Exiting game.");
-        return;
+        return null;
     }
 
-        return selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase();
-    };
+    return normalizedSelection.charAt(0).toUpperCase() + normalizedSelection.slice(1);
+}
 
-//Score counting and playing the game 5 times
+// Function to update the result and score
+function updateResultAndScore(result) {
+    const resultDiv = document.getElementById('resultDiv');
+    resultDiv.textContent = result;
 
+    const scoreDiv = document.getElementById('scoreDiv');
+    scoreDiv.textContent = `Your Score: ${playerScore} | Computer's Score: ${computerScore}`;
+
+    if (playerScore === 5) {
+        resultDiv.textContent = "Congratulations young padawan! You are the winner!";
+    } else if (computerScore === 5) {
+        resultDiv.textContent = "I win! I am still a Master.";
+    }
+}
+
+// Function to play the game
+// Function to play the game
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    while (true) {
+        playerScore = 0;
+        computerScore = 0;
 
-    const round1PlayerSelection = getUserSelection();
-    const round1ComputerSelection = getComputerChoice();
-    const round1Result = playRound(round1PlayerSelection, round1ComputerSelection);
-    console.log(round1Result);
-    if (round1Result.includes("You won")) {
+        while (playerScore < 5 && computerScore < 5) {
+            const playerSelection = getUserSelection();
+
+            if (playerSelection === null) {
+                return;
+            }
+
+            const computerSelection = getComputerChoice();
+            const roundResult = playRound(playerSelection, computerSelection);
+
+            if (roundResult.includes("You won")) {
+                playerScore++;
+            } else if (roundResult.includes("You need")) {
+                computerScore++;
+            }
+
+            updateResultAndScore(roundResult);
+
+            if (playerScore === 5 || computerScore === 5) {
+                break;
+            }
+        }
+
+        console.log("\nGame Over!");
+
+        if (playerScore > computerScore) {
+            console.log("Congratulations young padawan! You are the winner!");
+        } else {
+            console.log("I win! I am still a Master");
+        }
+
+        const playAgain = confirm("Do you want to play again?");
+        if (!playAgain) {
+            return;
+        }
+    }
+}
+
+// Event listeners for buttons
+document.getElementById('rockBtn').addEventListener('click', function () {
+    const playerSelection = 'Rock';
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    if (roundResult.includes("You won")) {
         playerScore++;
-    } else if (round1Result.includes("You need")) {
+    } else if (roundResult.includes("You need")) {
         computerScore++;
     }
 
-    const round2PlayerSelection = getUserSelection();
-    const round2ComputerSelection = getComputerChoice();
-    const round2Result = playRound(round2PlayerSelection, round2ComputerSelection);
-    console.log(round2Result);
-    if (round2Result.includes("You won")) {
-        playerScore++;
-    } else if (round2Result.includes("You need")) {
-        computerScore++;
+    updateResultAndScore(roundResult);
+
+    if (playerScore === 5 || computerScore === 5) {
+        game();
     }
-    
-    const round3PlayerSelection = getUserSelection();
-    const round3ComputerSelection = getComputerChoice();
-    const round3Result = playRound(round3PlayerSelection, round3ComputerSelection);
-    console.log(round3Result);
-    if (round3Result.includes("You won")) {
+});
+
+document.getElementById('paperBtn').addEventListener('click', function () {
+    const playerSelection = 'Paper';
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    if (roundResult.includes("You won")) {
         playerScore++;
-    } else if (round3Result.includes("You need")) {
+    } else if (roundResult.includes("You need")) {
         computerScore++;
     }
 
-    const round4PlayerSelection = getUserSelection();
-    const round4ComputerSelection = getComputerChoice();
-    const round4Result = playRound(round4PlayerSelection, round4ComputerSelection);
-    console.log(round4Result);
-    if (round4Result.includes("You won")) {
+    updateResultAndScore(roundResult);
+
+    if (playerScore === 5 || computerScore === 5) {
+        game();
+    }
+});
+
+document.getElementById('scissorsBtn').addEventListener('click', function () {
+    const playerSelection = 'Scissors';
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    if (roundResult.includes("You won")) {
         playerScore++;
-    } else if (round4Result.includes("You need")) {
+    } else if (roundResult.includes("You need")) {
         computerScore++;
     }
 
-    const round5PlayerSelection = getUserSelection();
-    const round5ComputerSelection = getComputerChoice();
-    const round5Result = playRound(round5PlayerSelection, round5ComputerSelection);
-    console.log(round4Result);
-    if (round5Result.includes("You won")) {
-        playerScore++;
-    } else if (round5Result.includes("You need")) {
-        computerScore++;
-    }
-    console.log("\nGame Over!");
-    console.log("Your score: " + playerScore);
-    console.log("Computer's score: " + computerScore);
+    updateResultAndScore(roundResult);
 
-    if (playerScore > computerScore) {
-        console.log("Congratulations young padawan!");
-    } else if (playerScore < computerScore) {
-        console.log("I win! I am still a Master");
-    } else {
-        console.log("It seems that we are equal");
+    if (playerScore === 5 || computerScore === 5) {
+        game();
     }
-};
+});
+
 
 game();
